2f57eb2e6442e9c8e2dfa2f9f9fccfcfbd88295e
Dmitry Ryabtsev <dmitry.ryabtsev.96@gmail.com>
2020-04-27 02:19:59 +0300

    improved test a bit

--- test/basic_tests.cpp
+++ test/basic_tests.cpp
@@ -1,9 +1,19 @@
 #include "ulp.hpp"
 
 #include <gtest/gtest.h>
+#include <array>
+#include <algorithm>
+#include <ios>
 
-TEST(basic, dumb_simple) {
-  double val = 1.3;
-  double ulp = ulp::calc_ulp<float>(val);
-  ASSERT_EQ(ulp, 0x1p-23);
+TEST(basic, flt) {
+  constexpr int num = 4;
+  std::array<double, num> val = {1.3, 0.0, 1.0, -5.0};
+  std::array<double, num> ref = {0x1p-23, 0x1p-149, 0x1p-23, 0x1p-21};
+  std::array<double, num> res;
+  std::transform(val.begin(), val.end(), res.begin(),
+    [](auto el) { return ulp::calc_ulp<float>(el); });
+  auto mism = std::mismatch(res.begin(), res.end(), ref.begin());
+  ASSERT_EQ(mism.first, res.end()) << std::hexfloat << "input " <<
+    val[mism.first - res.begin()] << " returned " << *mism.first <<
+    " expected " << *mism.second << std::endl;
 }
